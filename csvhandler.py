965cef401ea00679c317140cb026ec715ef3d990
jmari <jmarino1098@gmail.com>
2021-05-19 09:39:49 -0700

    Rewrote using Object

--- csvhandler.py
+++ csvhandler.py
@@ -1,29 +1,37 @@
 import csv
 
+class csv_file:
 
-def csv_read(inputfile):
-    info = {}
-    ids = []
-    ans = []
-    floats = []
-    with open(inputfile) as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            stripped = [col.replace('\n', ' ') for col in row]  # sanitizes input data
-            passed_data = []
-            for items in stripped:  # used to remove empty space
-                if items == '':
-                    pass
-                else:
-                    passed_data.append(items)
-            id_num = passed_data[1].strip("abcdefghijklmnopqrstuvwxyz.@")  # gets the ID number
-            num_ans = len(passed_data[2:])
-            ids.append(id_num)
-            ans.append(num_ans)
-        for values in ans[1:]:
-            div = float(values/ans[0])
-            #print('The div is ' + str(div))
-            floats.append(div)
-    info = dict(zip(ids[1:],floats[1:]))
-    return inputfile, info
+    def __init__(self,inputfile):
+        self.input = inputfile
+        self.info = {}
+
+    def read(self):
+        ids = []
+        ans = []
+        floats = []
+        with open(self.input, newline='',encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile,dialect='excel')
+            for row in reader:
+                passed_data = []
+                for items in row:  # used to remove empty space
+                    if items == '':
+                        pass
+                    else:
+                        passed_data.append(items)
+                id_num = passed_data[1].strip("abcdefghijklmnopqrstuvwxyz.@")  # gets the ID number
+                num_ans = len(passed_data[2:])
+                ids.append(id_num)
+                ans.append(num_ans)
+            for values in ans[1:]:
+                div = float(values/ans[0])
+                #print('The div is ' + str(div))
+                floats.append(div)
+        self.info = dict(zip(ids[1:],floats[1:]))
+
+    def print(self):
+        print(self.info)
+
+    def test(self):
+        print(self.input)
 
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ for file in os.listdir(test):
     if file.endswith(".csv"):
         list_of_csv.append(file)
 
-print(list_of_csv)  #Debug for testing list of files
+#print(list_of_csv)  #Debug for testing list of files
 
 for items in list_of_csv:
-    file = csv_read(items)
-    print('The file names is: ' + file[0].rstrip('.csv'))
+    file = csv_file(items)
+    file.read()
+    file.print()
